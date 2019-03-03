1f613ded62c0b162f0fc76eacea65341ec1a5dfc
Lars Kühne <lars.kuehne@dlr.de>
2019-03-03 20:11:54 +0100

    Modernize build tool chain

--- modelDecoder.cpp
+++ modelDecoder.cpp
@@ -1,20 +1,21 @@
+#include "modelDecoder.hpp"
+
 #include <cstdlib>
+
 #include <string>
 #include <vector>
 
-#include "zlib/zlib.h"
-
-#include "modelDecoder.hpp"
+#include <zlib.h>
 
 using std::string;
 using std::vector;
 
-string ModelDecoder::decodeModel(const vector<unsigned char>& codedModel) {
+string ModelDecoder::decodeModel(const vector<unsigned char> &codedModel) {
   uLongf decodedSize = 40000000; // 40 MB
-  Bytef* decodedBuffer = reinterpret_cast<Bytef*>(malloc(decodedSize));
-  uncompress OF((decodedBuffer, &decodedSize,
-                &codedModel[0], codedModel.size()));
-  char* pString = reinterpret_cast<char*>(decodedBuffer);
+  Bytef *decodedBuffer = reinterpret_cast<Bytef *>(malloc(decodedSize));
+  uncompress OF(
+      (decodedBuffer, &decodedSize, &codedModel[0], codedModel.size()));
+  char *pString = reinterpret_cast<char *>(decodedBuffer);
   // append the \0 char
   pString[decodedSize] = '\0';
   string result(pString);
